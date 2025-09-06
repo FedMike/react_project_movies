65d4ccb2241f2ed8e194f5e3dff0adaaf4d6ae3d
fedmike <molnek87@mail.ru>
2025-09-06 12:57:50 +0900

    corrected header and readme

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,18 @@
 function Header() {
-    return <nav className="green darken-1">
-        <div className="nav-wrapper">
-        <a href="#" className="brand-logo">React Movies</a>
+  return (
+    <nav className="green darken-1">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          React Movies
+        </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+          <li>
+            <a href="https://github.com/FedMike/react_project_movies">Repo</a>
+          </li>
         </ul>
-        </div>
+      </div>
     </nav>
-};
+  );
+}
 
-export {Header};
+export { Header };
